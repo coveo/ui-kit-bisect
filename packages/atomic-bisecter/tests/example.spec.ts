8c8a25bfe254a4c80a7e1d486c79e42b1d93558d
Louis Bompart <lbompart@coveo.com>
2024-02-15 15:50:49 -0500

    chore: remove spec

--- packages/atomic-bisecter/tests/example.spec.ts
+++ packages/atomic-bisecter/tests/example.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:3333/');
-
-  // Expect a title "to contain" a substring.
-  await page.getByRole('button', {name:'Sort & Filter'}).click();
-  await expect(page.getByTitle('Expand the Cost Range (auto) facet')).toHaveCount(2);
+  /**
+   * Write your test here
+   */
+  expect(true).toBeTruthy();
 });
